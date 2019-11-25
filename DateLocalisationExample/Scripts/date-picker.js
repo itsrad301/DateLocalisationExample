7a3ccf2cb326dfa5e13190783db3e6a6a140b79c
ITSPaul <powell.paul@itsligo.ie>
2019-11-25 23:53:50 +0000

    exploring setting date picker to vaue from editor for value

--- DateLocalisationExample/Scripts/date-picker.js
+++ DateLocalisationExample/Scripts/date-picker.js
@@ -1,6 +1,6 @@
 ﻿$(function () { // will trigger when the document is ready
         $('.date-picker').datepicker({
-        dateFormat: 'dd-MM-yy',
+        dateFormat: 'dd-M-yy',
         //startDate: thisDate
     }
 
