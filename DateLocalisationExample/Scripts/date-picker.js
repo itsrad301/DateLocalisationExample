0e7f6b9d44a4c532cb58909de9dab206351cbdcc
ITSPaul <powell.paul@itsligo.ie>
2019-11-25 13:12:03 +0000

    Updating Bootstrap Date Picker Format and layout for Boot strap version 4

--- DateLocalisationExample/Scripts/date-picker.js
+++ DateLocalisationExample/Scripts/date-picker.js
@@ -1,6 +1,7 @@
 ﻿$(function () { // will trigger when the document is ready
-    $('.date-picker').datepicker({
-        dateFormat: 'dd-M-yy'
+        $('.date-picker').datepicker({
+        dateFormat: 'dd-MM-yy',
+        //startDate: thisDate
     }
 
     ); //Initialise any date pickers
