4dd3d54621ea0b659fb76f0d330999aa16b7d81d
itspaul <powell.paul@itsligo.ie>
2022-11-14 16:10:50 +0000

    Demonstrating HTML 5 Date Picker and Bootstrap Date Pickers with date display format model attributes and overrides 2021. Updating Seeding method for Dates.

--- DateLocalisationExample/Migrations/Configuration.cs
+++ DateLocalisationExample/Migrations/Configuration.cs
@@ -16,13 +16,12 @@ namespace DateLocalisationExample.Migrations
 
         protected override void Seed(DateExample.DataModel.Week11Context context)
         {
-            CultureInfo cultureinfo = CultureInfo.CreateSpecificCulture("en-IE");
 
             context.Dates.AddOrUpdate(new LocalDate[] {
             new LocalDate
             {
-                StartDate = DateTime.ParseExact("25/03/2017","dd/MM/yyyy",cultureinfo),
-                EndDate = DateTime.ParseExact("01/02/2017","dd/MM/yyyy",cultureinfo),
+               StartDate = new DateTime(day:25,month:05,year:2022),
+                EndDate = new DateTime(day:26,month:05,year:2022),
 
             },
             });
